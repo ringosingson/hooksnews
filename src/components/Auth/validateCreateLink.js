5de741e991ad7b85cbb4d0f6e05269768fdd920d
Ringo Singson <ringosingson@live.com>
2019-08-09 08:05:46 +0530

    building createLink form with validation

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,18 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink(values) {
+  let errors = {};
+
+  //description errors
+  if (!values.description) {
+    errors.description = 'Description required';
+  } else if (values.description.length < 10) {
+    errors.description = 'Description must be atleast 10 characters';
+  }
+
+  //url errors
+  if (!values.url) {
+    errors.url = 'URL required';
+  } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
+    errors.url = 'URL must be valid';
+  }
+  return errors;
+}
