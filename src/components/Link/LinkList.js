72dcc408fdac71fe0a021c2867014ef58a73c6b6
Ringo Singson <ringosingson@live.com>
2019-08-10 08:17:09 +0530

    pagination and limit

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,37 +1,70 @@
 import React from 'react';
 import FirebaseContext from '../../firebase/context';
 import LinkItem from './LinkItem';
+import { LINKS_PER_PAGE } from '../../utils/index';
 
 function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext);
   const [links, setLinks] = React.useState([]);
-  //const isNewPage = props.location.pathname.includes('new');
+  const [cursor, setCursor] = React.useState(null);
+  const isNewPage = props.location.pathname.includes('new');
   const isTopPage = props.location.pathname.includes('top');
+  const page = Number(props.match.params.page);
 
   React.useEffect(() => {
     function getLinks() {
+      const hasCursor = Boolean(cursor);
       if (isTopPage) {
         return firebase.db
           .collection('links')
           .orderBy('voteCount', 'desc')
+          .limit(LINKS_PER_PAGE)
+          .onSnapshot(handleSnapshot);
+      } else if (page === 1) {
+        return firebase.db
+          .collection('links')
+          .orderBy('created', 'desc')
+
+          .limit(LINKS_PER_PAGE)
+          .onSnapshot(handleSnapshot);
+      } else if (hasCursor) {
+        return firebase.db
+          .collection('links')
+          .orderBy('created', 'desc')
+          .startAfter(cursor.created)
+          .limit(LINKS_PER_PAGE)
           .onSnapshot(handleSnapshot);
       }
-      return firebase.db
-        .collection('links')
-        .orderBy('created', 'desc')
-        .onSnapshot(handleSnapshot);
     }
 
     const unsubscribe = getLinks();
     return () => unsubscribe();
-  }, [firebase.db, isTopPage]);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [firebase.db, isTopPage, page]);
 
   function handleSnapshot(snapshot) {
     const links = snapshot.docs.map(doc => {
       return { id: doc.id, ...doc.data() };
     });
+    const lastLink = links[links.length - 1];
     setLinks(links);
+    setCursor(lastLink);
   }
+
+  function visitPreviousPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  }
+
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
+  }
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
 
   return (
     <div>
@@ -40,9 +73,20 @@ function LinkList(props) {
           key={link.id}
           showCount={true}
           link={link}
-          index={index + 1}
+          index={index + pageIndex}
         />
       ))}
+      {isNewPage && (
+        <div className='pagination'>
+          <div className='pointer mr2' onClick={visitPreviousPage}>
+            Previous
+          </div>
+          {' | '}
+          <div className='pointer ml2' onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
